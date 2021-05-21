03dc6f1dce388520127aced340a656fc9f73fa69
Ata Çetin <ihavenick@gmail.com>
2021-05-21 23:02:43 +0300

    STATE manager ve ölme falan

---
+++ Assets/Core/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public enum STATE
+{
+    RUN,
+    ATTACK,
+}
+
+public class GameManager : MonoBehaviour
+{
+    public STATE e_State = STATE.RUN;
+    
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Core/Scripts/PlayerMovement.cs
+++ Assets/Core/Scripts/PlayerMovement.cs
@@ -16,80 +16,100 @@ public class PlayerMovement : MonoBehaviour
     private LANE e_Lane = LANE.Mid;
     private float Xcordinate = 0f;
     public float Health = 100f;
-        
+
     private bool SwipeLeft;
     public float Speed = 7f;
     private bool SwipeRight;
     public float xValue;
     private CharacterController cc;
-    
-    
+    private GameManager GM;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
         cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        SwipeLeft = Input.GetKeyDown(KeyCode.A);
-        SwipeRight = Input.GetKeyDown(KeyCode.D);
-        float x1 = 0;
-        float x2;
-
-        if (Input.GetMouseButtonDown(0))
+        if (GM.e_State == STATE.RUN)
         {
-            x1 = Input.mousePosition.x;
+            SwipeLeft = Input.GetKeyDown(KeyCode.A);
+            SwipeRight = Input.GetKeyDown(KeyCode.D);
+            float x1 = 0;
+            float x2;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                x1 = Input.mousePosition.x;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                x2 = Input.mousePosition.x;
+
+                if (x1 > x2)
+                {
+                    SwipeLeft = true;
+                }
+
+                if (x2 > x1)
+                {
+                    SwipeRight = true;
+                }
+            }
+
+            if (SwipeLeft)
+            {
+                if (e_Lane == LANE.Mid)
+                {
+                    Xcordinate = -xValue;
+                    e_Lane = LANE.Left;
+                }
+                else if (e_Lane == LANE.Right)
+                {
+                    Xcordinate = 0;
+                    e_Lane = LANE.Mid;
+                }
+            }
+            else if (SwipeRight)
+            {
+                if (e_Lane == LANE.Mid)
+                {
+                    Xcordinate = xValue;
+                    e_Lane = LANE.Right;
+                }
+                else if (e_Lane == LANE.Left)
+                {
+                    Xcordinate = 0;
+                    e_Lane = LANE.Mid;
+                }
+            }
+
+            var x = Xcordinate - transform.position.x;
+            var vector = new Vector3(x, 0, Time.deltaTime * Speed);
+
+            cc.Move(vector);
         }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            x2 = Input.mousePosition.x;
-
-            if ( x1>x2)
-            {
-                SwipeLeft = true;
-            }
-
-            if (x2 > x1)
-            {
-                SwipeRight = true;
-            }
-        }
-        
-        if (SwipeLeft)
-        {
-            if (e_Lane == LANE.Mid)
-            {
-                Xcordinate = -xValue;
-                e_Lane = LANE.Left;
-            }
-            else if(e_Lane == LANE.Right)
-            {
-                Xcordinate = 0;
-                e_Lane = LANE.Mid;
-            }
-        }
-        else if (SwipeRight)
-        {
-            if (e_Lane == LANE.Mid)
-            {
-                Xcordinate = xValue;
-                e_Lane = LANE.Right;
-            }
-            else if(e_Lane == LANE.Left)
-            {
-                Xcordinate = 0;
-                e_Lane = LANE.Mid;
-            }
-        }
-
-        var x = Xcordinate - transform.position.x;
-        var vector = new Vector3(x, 0, Time.deltaTime * Speed);
-
-        cc.Move(vector);
     }
+
+    void Death()
+    {
+        Destroy(GameObject.Find("Player"));
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (Health < 0)
+        {
+            Death();
+        }
+    }
+
 
     private void OnCollisionEnter(Collision other)
     {
