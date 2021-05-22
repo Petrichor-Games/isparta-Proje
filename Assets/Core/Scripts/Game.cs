57c68d16d136d112397c3136f783c080b23ce29e
Ece Taş <ecetas1502@gmail.com>
2021-05-22 19:06:01 +0300

    MainMenu&Shop

---
+++ Assets/Core/Scripts/Game.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    #region SIngleton:Game
+    public static Game Instance;
+    void Awake()
+    {
+        if(Instance==null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    public int Coins;
+    public void UseCoins (int amout)
+    {
+        Coins -= amout;
+    }
+
+    public bool HasEnoughCoins (int amount)
+    {
+        return (Coins >= amount);
+    }
+}
